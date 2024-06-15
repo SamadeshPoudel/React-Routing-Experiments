3825925f0e95115428467c0fdfba53fb905cc57d
Samadesh Poudel <poudelsamadesh@gmail.com>
2024-06-15 10:55:42 +0530

    Just created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+import Landing from './components/Landing'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/landing' element={<Landing />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
